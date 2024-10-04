0fa340709aa4584718cde9c517f308b11e44c44f
ConcreteCactus <aron.harnasi@gmail.com>
2024-10-04 11:29:30 +0200

    visitor

--- clang-tools-extra/clang-tidy/bugprone/SideEffectsBetweenSequencePointsCheck.cpp
+++ clang-tools-extra/clang-tidy/bugprone/SideEffectsBetweenSequencePointsCheck.cpp
@@ -59,8 +59,38 @@ AST_MATCHER_P(CallExpr, hasFunctionFrom,
     return false;
 }
 
-AST_MATCHER_P(Stmt, twoOfBetweenSequencePoints,
-        ExpressionMatcher, InnerMatcher) {
+AST_MATCHER(Expr, twoGlobalWritesBetweenSequencePoints) {
+    const Expr* E = &Node;
+
+    const ast_matchers::internal::Matcher<Expr> GlobalMatcher = 
+        hasDescendant(declRefExpr(globalVarExpr()));
+
+    if(const BinaryOperator* Op = dyn_cast<BinaryOperator>(E)) {
+        const BinaryOperator::Opcode Code = Op->getOpcode();
+        if(Code == BO_LAnd || Code == BO_LOr || Code == BO_Comma) {
+            return false;
+        }
+
+        SideEffectsBetweenSequencePointsCheck::GlobalASTVisitor LeftVisitor;
+        SideEffectsBetweenSequencePointsCheck::GlobalASTVisitor RightVisitor;
+        LeftVisitor.TraverseStmt(Op->getLHS());
+        RightVisitor.TraverseStmt(Op->getRHS());
+        const std::vector<DeclarationName>& LeftFound = 
+            LeftVisitor.getGlobalsFound();
+        const std::vector<DeclarationName>& RightFound = 
+            RightVisitor.getGlobalsFound();
+        for(uint32_t I = 0; I < LeftFound.size(); I++) {
+            for(uint32_t J = 0; J < RightFound.size(); J++) {
+                if(LeftFound[I] == RightFound[J]) {
+                    std::cout << "Found with name: " 
+                              << LeftFound[I].getAsString()
+                              << std::endl;
+                }
+            }
+        }
+    }
+
+    return false;
 }
 
 SideEffectsBetweenSequencePointsCheck::SideEffectsBetweenSequencePointsCheck(
@@ -104,6 +134,28 @@ SideEffectsBetweenSequencePointsCheck::FunctionCallback::
 
 void SideEffectsBetweenSequencePointsCheck::FunctionCallback::run(
         const MatchFinder::MatchResult& Result) {
+}
+
+bool SideEffectsBetweenSequencePointsCheck::GlobalASTVisitor::
+    VisitStmt(Stmt* S) {
+    if(!isa<DeclRefExpr>(S)) {
+        return false;
+    }
+    const auto* DR = dyn_cast<DeclRefExpr>(S);
+    if(!isa<VarDecl>(DR->getDecl())) {
+        return false;
+    }
+    const auto* VD = dyn_cast<VarDecl>(DR->getDecl());
+    if(VD->hasGlobalStorage()) {
+        GlobalsFound.push_back(VD->getDeclName());
+        return true;
+    }
+    return false;
+}
+
+const std::vector<DeclarationName>& SideEffectsBetweenSequencePointsCheck::
+                                    GlobalASTVisitor::getGlobalsFound() {
+    return GlobalsFound;
 }
 
 } // namespace clang::tidy::bugprone
